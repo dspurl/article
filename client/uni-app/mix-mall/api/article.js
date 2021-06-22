769ea2ec2ee11af40fbe73821a35b53bcda2b29e
dspurl <49799121+dspurl@users.noreply.github.com>
2021-06-22 16:16:10 +0800

    扑充uni-app文章插件内容

--- client/uni-app/mix-mall/api/article.js
+++ client/uni-app/mix-mall/api/article.js
@@ -1,6 +1,6 @@
 import Network from '../utils/network.js'
-export function getList(data,success,fail) {
-	Network.setGet('article',data, function (res) {
+export function getList(id, data,success,fail) {
+	Network.setGet('article/column/' + id,data, function (res) {
 	  success(res)
 	}, function (res) {
 	  uni.showToast({
@@ -12,6 +12,17 @@ export function getList(data,success,fail) {
 }
 export function detail(id, data,success,fail) {
 	Network.setGet('article/' + id,data, function (res) {
+	  success(res)
+	}, function (res) {
+	  uni.showToast({
+		title: res.message,
+		icon: 'none',
+		duration: 2000
+	  })
+	})
+}
+export function pv(id, data,success,fail) {
+	Network.setPost('article/pv/' + id,data, function (res) {
 	  success(res)
 	}, function (res) {
 	  uni.showToast({
